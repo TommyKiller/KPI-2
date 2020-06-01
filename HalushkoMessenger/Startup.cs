d5bd626088c64ea761e816571e646acb2340d03a
Admin <Admin@DESKTOP-4A8333F>
2020-06-01 19:13:46 +0300

    add Register form

--- HalushkoMessenger/Startup.cs
+++ HalushkoMessenger/Startup.cs
@@ -47,6 +47,9 @@ namespace HalushkoMessenger
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            app.UseDefaultFiles();
+
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
@@ -54,6 +57,9 @@ namespace HalushkoMessenger
 
             app.UseAuthentication();
             app.UseAuthorization();
+
+           
+            
 
             app.UseEndpoints(endpoints =>
             {
