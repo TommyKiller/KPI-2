f56b63cd83645038fce70b04c8665d70bbb850a3
Nickolay <nicko6k9@yandex.ua>
2020-06-02 07:01:05 +0300

    TO DO: Authorization filter

---
+++ HalushkoMessenger/Filters/AuthorizeFilterAttribute.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HalushkoMessenger.Filters
+{
+    public class AuthorizeFilterAttribute : AuthorizeAttribute, IAsyncActionFilter
+    {
+        public Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- HalushkoMessenger/Startup.cs
+++ HalushkoMessenger/Startup.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HalushkoMessenger.Filters;
 using HalushkoMessenger.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,7 +28,7 @@ namespace HalushkoMessenger
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddMvc();
             services.AddDbContext<UserDbContext>();
             services.AddDbContext<AccountsDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("IdentityConnection")));
